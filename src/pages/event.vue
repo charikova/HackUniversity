bc888d40c8859858fb9aff46e311e7d7b5996396
Mariia Charikova <m.charikova@innopolis.ru>
2019-03-23 00:45:24 +0300

    Add buttons for vote

--- src/pages/event.vue
+++ src/pages/event.vue
@@ -1,7 +1,26 @@
 <template>
-  <f7-page>
-    <f7-navbar title="Мероприятие"></f7-navbar>
-  </f7-page>
+    <f7-page>
+      <f7-navbar title="Мероприятие"></f7-navbar>
+      <f7-block>
+        <f7-row>
+        <f7-col>
+          <f7-button fill color="red">Red</f7-button>
+        </f7-col>
+        <f7-col>
+          <f7-button fill color="red">Red</f7-button>
+        </f7-col>
+      </f7-row>
+        <br>
+        <f7-row>
+          <f7-col>
+            <f7-button fill color="red">Red</f7-button>
+          </f7-col>
+          <f7-col>
+            <f7-button fill color="red">Red</f7-button>
+          </f7-col>
+        </f7-row>
+      </f7-block>
+    </f7-page>
 </template>
 
 <script>
