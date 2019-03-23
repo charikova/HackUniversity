da0bdc22dd2473ee79972703624e9b47834f20dd
alevs2r <a.evlampev@dooglys.com>
2019-03-23 12:43:14 +0300

    добавил главную страницу

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,19 +1,18 @@
 <template>
   <f7-page class="main-page">
-    <div class="main-block" style="background: #ff3b30;">
-      <div class="main-layout-block"
-      ></div>
-
-    </div>
     <div class="main-block">
-      <div class="main-layout-block"
-      >
-          <f7-button big fill color="red" @click="start">Начать мероприятие</f7-button>
+      <div class="img-layer">
+        <img src="./../img/concert.jpg" class="concert-img" />
       </div>
     </div>
     <div class="main-block">
-      <div class="main-layout-block"
-      ></div>
+      <div class="main-layout-block">
+        <div class="description">
+          <div class="header">Узнайте лучше свою аудиторию!</div>
+          Запустите голосование и дайте возможность посетителям концертов самим определить следующую песню
+        </div>
+        <f7-button big fill color="red" @click="start">Начать мероприятие</f7-button>
+      </div>
     </div>
   </f7-page>
 </template>
@@ -36,15 +35,43 @@
     flex-direction: column;
   }
 
+  .main-page {
+    background: #000 !important;
+  }
+
   .main-block {
     width: 100%;
-    flex-grow: 1;
-    border: 1px black solid;
   }
 
   .main-layout-block {
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    height: 100%;
+    box-sizing: border-box;
+    background: #000;
+    color: #fff;
+  }
+
+  .concert-img {
     width: 100%;
-    background: rgba(0, 0, 0, 0.2);
+  }
+
+  .img-layer {
+    background: rgba(0,0,0,.2);
+  }
+
+  .description {
+    font-size: 1.1rem;
+    margin-bottom: 20px;
+  }
+
+  .header {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin-bottom: 10px;
+    line-height: 1.1;
   }
 
 </style>
