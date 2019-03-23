615083c18e320bf817da048a29c1c6331e765ea7
alevs2r <a.evlampev@dooglys.com>
2019-03-24 00:27:51 +0300

    finish paysheet

--- src/components/PaySheet.js.vue
+++ src/components/PaySheet.js.vue
@@ -11,12 +11,12 @@
       <f7-block v-if="track">
         <p class="trackname">{{ `Вы голосуете за ${track.name} – ${track.title}` }}</p>
         <f7-row>
-          <f7-col><button class="col button button-outline">rtr</button></f7-col>
-          <f7-col><button class="col button button-outline">rtr</button></f7-col>
+          <f7-col><button class="col button button-outline">50 руб.<br />+ 1 голосов</button></f7-col>
+          <f7-col><button class="col button button-outline">100 руб.<br />+ 5 голосов</button></f7-col>
         </f7-row>
         <f7-row>
-          <f7-col><button class="col button button-outline"></button></f7-col>
-          <f7-col><button class="col button button-outline"></button></f7-col>
+          <f7-col><button class="col button button-outline">500 руб.<br />+ 25 голосов</button></f7-col>
+          <f7-col><button class="col button button-outline">1000 руб.<br />+ 50  голосов</button></f7-col>
         </f7-row>
       </f7-block>
     </f7-page-content>
@@ -33,5 +33,13 @@
 <style scoped>
   .trackname {
     font-size: 1.2rem;
+  }
+  .button {
+    font-size: 1.2rem;
+    margin-bottom: 15px;
+    height: 60px;
+  }
+  .sheet-modal {
+    height: 320px;
   }
 </style>
